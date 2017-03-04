6d5ed63571cdb139efc2ef3a17c50c31ef901c27
Christian Manalansan <christian.manalansan@cgifederal.com>
2017-03-04 02:18:51 -0500

    Create initial page for RPS game, stub 'quickStartGame' command

---
+++ client/GameObjects/RPSRefereeReceiver.js
@@ -0,0 +1,7 @@
+/**
+ * Created by cmanalan on 3/4/2017.
+ */
+
+var RPSRefereeReceiver = {
+
+}
---
+++ server/src/GameServer/GameHostess.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cmanalan
+ * Date: 3/4/2017
+ * Time: 2:14 AM
+ */
+
+namespace GameServer;
+
+
+class GameHostess implements GameServerHandler {
+  public function handle($msg) {
+    if ($msg->operation == "quickStartGame") {
+      // try to find a game with an empty seat and load it. otherwise, start a new game.
+    }
+  }
+}
