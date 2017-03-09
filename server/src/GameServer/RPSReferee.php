0ba6571bb2452b32033d80df2ea876dc8ac5d624
Christian Manalansan <christian.manalansan@cgifederal.com>
2017-03-09 03:30:48 -0500

    Broadcast game status to other people in the game

--- server/src/GameServer/RPSReferee.php
+++ server/src/GameServer/RPSReferee.php
@@ -13,26 +13,25 @@ class RPSReferee extends GameServerHandler {
   public $gameState;
 
   public function handle($msg) {
+    // no matter what we are doing, start by getting the game state
     $this->getGameState($msg->gameId);
 
     if ($msg->operation == "fetchGameState") {
       if ($this->gameState) {
+        $this->sender->player = $msg->player;
         $this->sendGameState($msg->player);
       }
     }
 
     if ($msg->operation == "makeMove") {
-      $this->sendGameState($msg->player);
+      // validate if move is allowed
 
-      $this->sender->send(json_encode(array(
-        'from' => 'RPSReferee',
-        'operation' => 'say',
-        'content' => array(
-          'sender' => "RPSReferee",
-          'mode' => 'private',
-          'message' => "A move has been made.",
-        ),
-      )));
+      // resolve combat?
+
+      // update database
+
+      // send updated game state
+      $this->sendGameState($msg->player);
     }
   }
 
@@ -40,6 +39,7 @@ class RPSReferee extends GameServerHandler {
     if ($result = $this::queryGameState($gameId)) {
       $gameStateJSON = current($result->fetch(\PDO::FETCH_ASSOC));
       $this->gameState = \GuzzleHttp\json_decode($gameStateJSON);
+      $this->sender->gameId = $gameId;
     } else {
       $this->sender->send(json_encode(array(
         'from' => 'RPSReferee',
@@ -55,21 +55,30 @@ class RPSReferee extends GameServerHandler {
   }
 
   public function sendGameState($player) {
-    if ($this->gameState->player1->name == $player) {
-      if (!empty($this->gameState->currentRound->p2)) {
-        $this->gameState->currentRound->p2 = "finished and waiting";
-      }
-    }
-    if ($this->gameState->player2->name == $player) {
-      if (!empty($this->gameState->currentRound->p1)) {
-        $this->gameState->currentRound->p1 = "finished and waiting";
-      }
-    }
+    foreach ($this->clients as $client) {
+      // only update clients who are in the game
+      if ($client->gameId == $this->sender->gameId) {
+        // hide opponent's selection
+        $playerGameState = unserialize(serialize($this->gameState)); // dirty stdClass clone - @see http://stackoverflow.com/questions/15945837/trying-to-clone-a-stdclass
+        if ($this->gameState->player1->name == $client->player) {
+          // this is player 1, so hide player 2's selection
+          if ($this->gameState->currentRound->p2 != "deciding...") {
+            $playerGameState->currentRound->p2 = "finished and waiting";
+          }
+        }
+        if ($this->gameState->player2->name == $client->player) {
+          // this is player 2, so hide player 1's selection
+          if ($this->gameState->currentRound->p1 != "deciding...") {
+            $playerGameState->currentRound->p1 = "finished and waiting";
+          }
+        }
 
-    $this->sender->send(json_encode(array(
-      'from' => 'RPSReferee',
-      'operation' => 'gameState',
-      'content' => $this->gameState,
-    )));
+        $client->send(json_encode(array(
+          'from' => 'RPSReferee',
+          'operation' => 'gameState',
+          'content' => $playerGameState,
+        )));
+      }
+    }
   }
 }
