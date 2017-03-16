f2855fda7ba9cdc80920fc4e9f969a8f3a23d4bd
Christian Manalansan <christian.manalansan@cgifederal.com>
2017-03-16 08:04:22 -0400

    Reduce database calls

--- server/src/GameServer/GameHostess.php
+++ server/src/GameServer/GameHostess.php
@@ -40,10 +40,7 @@ class GameHostess extends GameServerHandler {
         $this->sender->send(json_encode(array(
           'from' => 'GameHostess',
           'operation' => 'getGames',
-          'content' => array(
-            'myGames' => $this->getMyGames(),
-            'openGames' => $this->getOpenGames(),
-          ),
+          'content' => $this->getRelevantGames(),
         )));
       } else {
         $this->sender->send(json_encode(array(
@@ -59,55 +56,48 @@ class GameHostess extends GameServerHandler {
     }
   }
 
-  public function getMyGames() {
+  /**
+   * Return a list of games that the user is currently playing. Also, return any
+   * games with empty seats that they can join
+   */
+  private function getRelevantGames() {
+    // query the database
     $dir = 'sqlite:C:\Apache24\htdocs\rps-game\server\db\game.db';
     $dbh = new \PDO($dir) or die("cannot open the database");
     $query = $dbh->prepare('SELECT * FROM games');
-
     $query->execute(array());
-    /**
-     * "To return an associative array grouped by the values of a specified
-     * column, bitwise-OR PDO::FETCH_COLUMN with PDO::FETCH_GROUP"
-     *
-     * @see http://php.net/manual/en/pdostatement.fetchall.php
-     */
     $results = $query->fetchAll();
-    $games = array();
+
+    // process the results
+    $games = array(
+      'myGames' => array(),
+      'openGames' => array(),
+    );
     foreach ($results as $value) {
       $gameObj = \GuzzleHttp\json_decode($value['game_state']);
       if (!$gameObj->isGameOver) {
+
+        // currently playing this game
         if (
           $gameObj->player1->name == $this->sender->name
           || $gameObj->player2->name == $this->sender->name
         ) {
-          $games[$value['game_id']] = $gameObj;
+          $games['myGames'][$value['game_id']] = $gameObj;
           continue;
         }
-      }
-    }
-    return $games;
-  }
 
-  public function getOpenGames() {
-    $dir = 'sqlite:C:\Apache24\htdocs\rps-game\server\db\game.db';
-    $dbh = new \PDO($dir) or die("cannot open the database");
-    $query = $dbh->prepare('SELECT * FROM games');
-
-    $query->execute(array());
-    $results = $query->fetchAll();
-    $games = array();
-    foreach ($results as $value) {
-      $gameObj = \GuzzleHttp\json_decode($value['game_state']);
-      if (!$gameObj->isGameOver) {
+        // this game has an empty seat
         if (
           $gameObj->player1->name == FALSE
           || $gameObj->player2->name == FALSE
         ) {
-          $games[$value['game_id']] = $gameObj;
+          $games['openGames'][$value['game_id']] = $gameObj;
           continue;
         }
+
       }
     }
+
     return $games;
   }
 }
